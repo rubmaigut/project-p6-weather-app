dc646e27c84fdc1dc29c0dfe23bae51c83a49df4
Matilda Arvidsson <51529201+HIPPIEKICK@users.noreply.github.com>
2020-11-16 18:14:57 +0100

    Add files via upload

---
+++ code/weather.js
@@ -0,0 +1,15 @@
+//HTML elements
+const shortDescription = document.getElementById("shortDescription")
+const body = document.getElementById("body")
+const temperature = document.getElementById("temperature")
+const sunrise = document.getElementById("sunrise")
+const sunset = document.getElementById("sunset")
+const description = document.getElementById("description")
+const forecast = document.getElementById("forecast")
+const icon = document.getElementById("icon")
+
+//Begin with fetching the data and logging it to the console.
+//Take a look at the data and look for what you need. Maybe start
+//with the short description and the temperature. After you've found
+//what you were looking for, you can change the innerHTML of the
+//elements in the top of this file
